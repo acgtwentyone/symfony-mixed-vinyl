b5b51de118343c6b7c47734ad5ae937bf3dd7948
António Gonçalves <acgtwentyone@gmail.com>
2022-06-11 16:28:34 -0100

    Add some twig templates

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,12 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class VinylController 
+class VinylController extends AbstractController
 {
     #[Route('/')]
-    public function homepage() 
+    public function homepage(): Response
     {
-        dd('Hite here');
+        return $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => [
+                'Track 1',
+                'Track 2', 'Track 3',
+            ]
+        ]);
     }
 }
